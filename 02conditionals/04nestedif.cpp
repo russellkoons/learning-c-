da0ed392a1b3900fee66eff8cc5465580879c4e2
Russell Koons <rkoons@gmail.com>
2019-05-02 14:21:38 -0400

    nested ifs

---
+++ 02conditionals/04nestedif.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int a, b, c;
+
+  cout << "Enter 3 numbers" << endl;
+  cin >> a >> b >> c;
+
+  if (a > b && a > c)
+    cout << "Max is " << a << endl;
+  else
+  {
+    if (b < c)
+      cout << "Max is " << c << endl;
+    else
+    {
+      cout << "Max is " << b << endl; 
+    }
+  }
+}
