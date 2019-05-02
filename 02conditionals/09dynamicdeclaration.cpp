7e347aefe905f836d72577d013919cc0c3132c5e
Russell Koons <rkoons@gmail.com>
2019-05-02 15:16:40 -0400

    dynamic declaration

---
+++ 02conditionals/09dynamicdeclaration.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+// You can declare variables anywhere you like!
+
+using namespace std;
+
+// int main()
+// {
+//   int a, b, c;
+
+//   if ()
+//   {
+//     int m;
+    // m only exists in this block
+//   }
+//   if ()
+//   {
+//     int x;
+//   }
+// }
+
+int main()
+{
+  int a, b, c;
+
+  if (int k = b; k < a)
+  {
+    // After if statement ends k disappears
+  }
+}
