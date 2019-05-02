7ee36fe95291a7ca1ffb3972ef8ddd7d67ff2796
Russell Koons <rkoons@gmail.com>
2019-05-02 16:19:21 -0400

    for loops

---
+++ 03loops/01forloop.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int n;
+  cout << "Enter a number: ";
+  cin >> n;
+  
+  for (int i = 0; i < n; i++)
+  {
+    cout << i + 1 << endl;
+  }
+}
