98ceb86f23c38817f8e61b3e2e8c7e728bf4218f
Russell Koons <rkoons@gmail.com>
2019-05-02 15:30:42 -0400

    switch case

---
+++ 02conditionals/10switchcase.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int day;
+  cout << "Enter day number";
+  cin >> day;
+
+  switch(day)
+  {
+    case 1: cout << "Mon";
+      break;
+    case 2: cout << "Tue";
+      break;
+    case 3: cout << "Wed";
+      break;
+    case 4: cout << "Thur";
+      break;
+    case 5: cout << "Fri";
+      break;
+    case 6: cout << "Sat";
+      break;
+    case 7: cout << "Sun";
+      break;
+    default: cout << "Invalid Number";
+  }
+}
