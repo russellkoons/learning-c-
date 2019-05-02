42a163aa89bba89100d03e7c3e51fff6feb00532
Russell Koons <rkoons@gmail.com>
2019-05-02 10:19:32 -0400

    variables

---
+++ 01basics/06variables.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int rollno = 10; // valid
+  // int roll no; invalid
+  int roll_no; // valid
+  int x1; // valid
+  // int 1x; invalid
+  char group = 'A';
+  float price = 12.75f;
+  return 0;
+}
+
+// Char types must be in ''
+// Strings are in ""
+// Write f after floats or it will think it's a double
+// can use camelCase
+// standard is CamelCase
