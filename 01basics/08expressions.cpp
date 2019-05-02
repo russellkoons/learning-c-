5cb60801820c7cdae5ff76037303db8d0c715bb4
Russell Koons <rkoons@gmail.com>
2019-05-02 11:02:05 -0400

    area of triangle program

---
+++ 01basics/08expressions.cpp
@@ -0,0 +1,45 @@
+// Expressions
+
+// Order of operators
+  // ()    1
+  // * / % 2
+  // + -   3
+
+  // x = a + b * c - d / e
+    // b * c first
+    // d / e second
+    // a + b third
+    // c - d fourth
+
+  // x = (a + b) * (c - d) / e
+    // (a + b) first
+    // (c - d) second
+    // () * () third
+    // / e last
+
+// Area of triangle
+  // a = 1/2bh can't work!
+  // a = (b * h) / 2 That's better
+  // a must be a float
+
+// Perimeter of Rectangle
+  // p = 2(l + b)
+  // p = 2 * (l + b)
+
+// Sum of n terms
+  // s=n(n+1)/2
+  // s = n * (n + 1) / 2
+
+// nth term of A*P Series
+  // t = a + (n-1)d
+  // t = a + (n - 1) * d
+
+  // sqrt() is the square root function
+
+// Quadratic
+  // x = (-b + sqrt(b * b - 4 * a * c))/(2 * a)
+
+// speed
+  // s = (v * v - u * u) / (2 * a)
+  // s = (pow(v, 2) - pow(u, 2))/ (2 * a)
+
---
+++ 01basics/09areatriangle.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  float a, b, h;
+  cout << "Enter base and height\n";
+  cin >> b >> h;
+  a = (b * h) / 2;
+  cout << "The area is " << a << "\n";
+  return 0;
+}
