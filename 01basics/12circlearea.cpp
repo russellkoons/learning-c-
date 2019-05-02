de5e1a2c5520620e9f5055cc4b1713f62f817f84
Russell Koons <rkoons@gmail.com>
2019-05-02 12:00:31 -0400

    circle area

---
+++ 01basics/12circlearea.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  float r, area;
+  cout << "Enter radius\n";
+  cin >> r;
+  area = 22/7.0 * r * r;
+  cout << "Area is " << area << endl;
+  return 0;
+}
