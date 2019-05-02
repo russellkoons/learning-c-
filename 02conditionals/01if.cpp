66b7a170a7a0c1bf7bffc3c9f32f0164a758956f
Russell Koons <rkoons@gmail.com>
2019-05-02 13:37:01 -0400

    if else

---
+++ 02conditionals/01if.cpp
@@ -0,0 +1,21 @@
+// If Else statements!
+// False = 0; True = any number except 0
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int x, y;
+  cout << "Enter 2 Numbers" << endl;
+  cin >> x >> y;
+  if (x > y)
+  {
+    cout << "Max is " << x << endl;
+  }
+  else
+  {
+    cout << "Max is " << y << endl;
+  }
+  
+}
