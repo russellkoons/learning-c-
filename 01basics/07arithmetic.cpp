2bae64024d280ec9ad6bc2f0840d638e1f310b46
Russell Koons <rkoons@gmail.com>
2019-05-02 10:39:37 -0400

    arithmetic operators

--- 01basics/07arithmetic.cpp
+++ 01basics/07arithmetic.cpp
@@ -1,1 +1,31 @@
-// Operators and Expressions
+// // Operators and Expressions
+
+// // Arithmetic - +, -, *, /, %
+
+// int a, b, c;
+
+// a = 10;
+// b = 5;
+// c = a + b; // 15 addition
+// c = a - b; // 5 subtraction
+// c = a * b; // 50 multiplication
+// c = b / a; // 2 division
+// c = b % a; // 0 modulo or remainder
+
+// // If you divide with an integer it will not make a float so 13 / 5 = 2
+// // mod gets remainder so 13 % 5 = 3
+
+// int a = 13, b = 5;
+
+// float c;
+// c = a / b;
+
+// float a = 13.5, b = 4.1, c;
+// // Cannot perform mods on float
+// c = a / b;
+
+// char a = 13, b = 5, c;
+
+// c = a % b;
+
+// // You can store numbers in char because characters are still numbers 'A' = 65 etc
