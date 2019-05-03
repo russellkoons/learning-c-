1d71831969abeccf6bba276eed2999af35ff2fc5
Russell Koons <rkoons@gmail.com>
2019-05-03 13:03:00 -0400

    functions intro

--- 06functions/01intro.cpp
+++ 06functions/01intro.cpp
@@ -14,10 +14,27 @@ int add(int x, int y)
   return z;
 }
 
+int maxim(int a, int b, int c) {
+  if (a > b && a > c)
+    return a;
+  else if (b > c)
+    return b;
+  else
+    return c;
+}
+
 int main()
 {
   display();
+
   int a = 10, b = 15, c;
   c = add(a, b);
   cout << "Sum is " << c << endl;
+
+  int x = 10, y = 15, z = 5, r;
+  r = maxim(x, y, z);
+  
+  cout << "Maximum is " << r << endl;
+
+  return 0;
 }
