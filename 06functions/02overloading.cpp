992709bb382daacfab502839a5df2692c2d02211
Russell Koons <rkoons@gmail.com>
2019-05-03 13:07:25 -0400

    overloading a function

--- 06functions/02overloading.cpp
+++ 06functions/02overloading.cpp
@@ -2,7 +2,32 @@
 
 using namespace std;
 
+int add(int x, int y)
+{
+  return x + y;
+}
+
+int add(int x, int y, int z)
+{
+  return x + y + z;
+}
+
+float add(float x, float y)
+{
+  return x + y;
+}
+
 int main()
 {
-  
+  int a = 10, b = 5, c, d;
+
+  c = add(a, b);
+  cout << c << endl;
+
+  d = add(a, b, c);
+  cout << d << endl;
+
+  float i = 2.5f, j = 3.5f, k;
+  k = add(i, j);
+  cout << k << endl;
 }
