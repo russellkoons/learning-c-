f1512d71a780959cbf787669b518f44618a6683f
Russell Koons <rkoons@gmail.com>
2019-05-03 09:40:23 -0400

    drawing shapes with loops

--- 04arrays/09drawing2.cpp
+++ 04arrays/09drawing2.cpp
@@ -4,5 +4,25 @@ using namespace std;
 
 int main()
 {
-  
+  for (int i = 0; i < 4; i++)
+  {
+    for (int j = 0; j < 4; j++)
+    {
+      if (j <= i)
+        cout << "*";
+    }
+    cout << endl;
+  }
+
+  for (int i = 0; i < 4; i++)
+  {
+    for (int j = 0; j < 4; j++)
+    {
+      if (j >= i)
+        cout << "*";
+      else
+        cout << " ";
+    }
+    cout << endl;
+  }
 }
--- 04arrays/10drawing3.cpp
+++ 04arrays/10drawing3.cpp
@@ -4,5 +4,17 @@ using namespace std;
 
 int main()
 {
-  
+  int n = 6;
+
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+      if (i + j >= n - 1)
+        cout << "*";
+      else
+        cout << " ";
+    }
+    cout << endl;
+  }
 }
