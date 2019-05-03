a03e5a00b96fe911f8f9b3f7e8a8385dd01da9a2
Russell Koons <rkoons@gmail.com>
2019-05-03 10:56:34 -0400

    heap allocation

--- 05pointers/03heapallocation.cpp
+++ 05pointers/03heapallocation.cpp
@@ -4,5 +4,21 @@ using namespace std;
 
 int main()
 {
-  
+  // int A[5] = {1, 2, 3, 4, 5};
+  // int *p;
+  // p = new int[5]; // This creates an array in the Heap as opposed to the main
+  // A[2] = 15;
+  // p[2] = 15;
+  // cout << p[2];
+  // cout << sizeof A;
+
+  // delete []p;
+  // return 0;
+
+  int *p = new int[20];
+  p[1] = 17;
+  delete []p; // Deletes the pointer array
+
+  p = new int[40];
+  cout << p[1];
 }
