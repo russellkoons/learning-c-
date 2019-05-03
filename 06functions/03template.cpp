61213164ca307f554749bc22a267ce440af0bd7e
Russell Koons <rkoons@gmail.com>
2019-05-03 13:38:34 -0400

    templates

--- 06functions/03template.cpp
+++ 06functions/03template.cpp
@@ -2,7 +2,17 @@
 
 using namespace std;
 
+template <class T>
+
+T Max(T a, T b)
+{
+  return a > b ? a : b;
+}
+
 int main()
 {
-  
+  int a = 10, b = 5;
+  int c = Max(a, b);
+
+  cout << c << endl;
 }
