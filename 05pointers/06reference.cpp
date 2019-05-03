a754756f5efc9d3ce2952f5516684029a5f7aa04
Russell Koons <rkoons@gmail.com>
2019-05-03 11:39:29 -0400

    references

---
+++ 05pointers/06reference.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int x = 10;
+  int &y = x;
+
+  cout << x << endl;
+
+  x++;
+  y++;
+
+  cout << x << endl;
+  cout << &x << " " << &y << endl;
+}
