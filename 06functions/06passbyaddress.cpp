0119f39ad84b748e94dffc970f7fd19666110028
Russell Koons <rkoons@gmail.com>
2019-05-03 14:06:26 -0400

    pass by address

--- 06functions/06passbyaddress.cpp
+++ 06functions/06passbyaddress.cpp
@@ -2,7 +2,19 @@
 
 using namespace std;
 
+// You can swap the variables using pointers!
+
+void swap(int *a, int *b)
+{
+  int temp;
+  temp = *a;
+  *a = *b;
+  *b = temp; 
+}
+
 int main()
 {
-  
+  int x = 10, y = 20;
+  swap(&x, &y);
+  cout << x << " " << y << endl;
 }
