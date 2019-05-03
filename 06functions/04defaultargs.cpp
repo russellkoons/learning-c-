2cf1c93d320a75fd38d6d244e800ee4577496a8c
Russell Koons <rkoons@gmail.com>
2019-05-03 13:43:01 -0400

    default args

--- 06functions/04defaultargs.cpp
+++ 06functions/04defaultargs.cpp
@@ -2,7 +2,18 @@
 
 using namespace std;
 
+int add(int x, int y, int z = 0)
+{
+  return x + y + z;
+}
+
 int main()
 {
+  int c = add(2, 5);
+
+  cout << c << endl;
+
+  c = add(2, 5, 8);
   
+  cout << c << endl;
 }
