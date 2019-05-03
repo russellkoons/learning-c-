65773c7fc8c17191343990d3b1a381d103415cb6
Russell Koons <rkoons@gmail.com>
2019-05-03 15:20:41 -0400

    return by address and reference

--- 06functions/08returnbyaddress.cpp
+++ 06functions/08returnbyaddress.cpp
@@ -2,7 +2,19 @@
 
 using namespace std;
 
+// Creates an array in the heap and then passes it to the main as opposed to deleting it
+
+int* fun(int size)
+{
+  int *p = new int[size];
+  for (int i = 0; i < size; i++)
+  {
+    p[i] = i + 1;
+  }
+  return p;
+}
+
 int main()
 {
-  
+  int *ptr = fun(5);
 }
--- 06functions/09returnbyref.cpp
+++ 06functions/09returnbyref.cpp
@@ -2,7 +2,15 @@
 
 using namespace std;
 
+int& fun(int& x)
+{
+  return x;
+}
+
 int main()
 {
-  
+  int a = 10;
+  fun(a) = 25;
+
+  cout << a << endl;
 }
