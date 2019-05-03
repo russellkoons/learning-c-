70e4b02930853e33076b76bba1ca5667aed54727
Russell Koons <rkoons@gmail.com>
2019-05-03 14:07:41 -0400

    pass by reference

--- 06functions/07passbyref.cpp
+++ 06functions/07passbyref.cpp
@@ -2,7 +2,19 @@
 
 using namespace std;
 
+// Also swaps the variables! Way easier than pointers!
+
+void swap(int &a, int &b)
+{
+  int temp;
+  temp = a;
+  a = b;
+  b = temp;
+}
+
 int main()
 {
-  
+  int x = 10, y = 20;
+  swap(x, y);
+  cout << x << " " << y << endl;
 }
