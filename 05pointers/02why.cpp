d55dd7faa446a4f855fb5e3924b8dcd760cd50e5
Russell Koons <rkoons@gmail.com>
2019-05-03 10:41:01 -0400

    why pointers

--- 05pointers/02why.cpp
+++ 05pointers/02why.cpp
@@ -4,5 +4,6 @@ using namespace std;
 
 int main()
 {
-  
+  // The program can only access the stack and the code
+  // Pointers can access the Heap, outside files, network connections, various hardwares
 }
