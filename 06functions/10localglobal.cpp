da72c7d535d7e2f1cba905ee2a9514ba4276697b
Russell Koons <rkoons@gmail.com>
2019-05-03 15:27:10 -0400

    local and global variables

--- 06functions/10localglobal.cpp
+++ 06functions/10localglobal.cpp
@@ -2,7 +2,19 @@
 
 using namespace std;
 
+int g = 0; // Global variable. Always accessible
+
+int fun()
+{
+  int a = 5; // Local variable. Only accessible within the function.
+  g = g + a;
+  cout << g << endl;
+}
+
 int main()
 {
-  
+  g = 15;
+  fun();
+  g++;
+  cout << g << endl;
 }
