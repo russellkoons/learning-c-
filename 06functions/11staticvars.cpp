5bba266215a5e3411e0fac674c67f60de485e4b9
Russell Koons <rkoons@gmail.com>
2019-05-03 15:46:14 -0400

    static vars and recursion

--- 06functions/11staticvars.cpp
+++ 06functions/11staticvars.cpp
@@ -2,7 +2,17 @@
 
 using namespace std;
 
+int fun() 
+{
+  static int v = 0; // Keeps the variable from being deleted but can't be accessed by other fns
+  int a = 5;
+  v++;
+  cout << a << " " << v << endl;
+}
+
 int main()
 {
-  
+  fun();
+  fun();
+  fun();
 }
--- 06functions/12recursive.cpp
+++ 06functions/12recursive.cpp
@@ -2,7 +2,17 @@
 
 using namespace std;
 
+void fun(int n)
+{
+  if (n > 0)
+  {
+    cout << n << endl;
+    fun(n - 1); // Calls the function until n = 0. Since it calls itself it is recursive
+  }
+}
+
 int main()
 {
-  
+  int x = 5;
+  fun(x);
 }
