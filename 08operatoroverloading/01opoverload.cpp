51c4e5b19ef40f5efe76e35ca4b97fb663fb78c1
Russell Koons <rkoons@gmail.com>
2019-05-14 16:57:47 -0400

    operator overloading

--- 08operatoroverloading/01opoverload.cpp
+++ 08operatoroverloading/01opoverload.cpp
@@ -19,7 +19,15 @@ class Complex
       img = i;
     }
 
-    Complex add(Complex x)
+    Complex add(Complex x) // These two functions do the exact same thing but are called
+    {                      // but are called in different ways
+      Complex temp;
+      temp.real = real + x.real;
+      temp.img = img + x.img;
+      return temp;
+    }
+
+    Complex operator+(Complex x)
     {
       Complex temp;
       temp.real = real + x.real;
@@ -33,4 +41,5 @@ int main()
   Complex c1(3, 7);
   Complex c2(5, 4);
   Complex c3 = c1.add(c2);
+  Complex c4 = c3 + c2; // This will work the same way because of the operator+ function
 }
