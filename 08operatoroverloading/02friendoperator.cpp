8e29cf31fe9170d77ddebeebc9e6b5cb65cafaf1
Russell Koons <rkoons@gmail.com>
2019-05-14 17:19:20 -0400

    insertion operators

--- 08operatoroverloading/02friendoperator.cpp
+++ 08operatoroverloading/02friendoperator.cpp
@@ -2,7 +2,46 @@
 
 using namespace std;
 
+// Friend Functions
+
+class Complex
+{
+  private:
+
+    int real;
+    int img;
+
+  public:
+
+    Complex(int r = 0, int i = 0)
+    {
+      real = r;
+      img = i;
+    }
+
+    void display()
+    {
+      cout << real << " + i" << img << endl;
+    }
+
+    friend Complex operator+(Complex c1, Complex c2);
+};
+
+// Since the functions is classified a friend it has access to private data in the class
+
+Complex operator+(Complex c1, Complex c2)
+{
+  Complex t;
+  t.real = c1.real + c2.real;
+  t.img = c1.img + c2.img;
+  return t;
+}
+
 int main()
 {
-  
+  Complex c1(5, 10);
+  Complex c2(6, 9);
+  Complex c3 = c1 + c2;
+
+  c3.display();
 }
--- 08operatoroverloading/03insertionoperator.cpp
+++ 08operatoroverloading/03insertionoperator.cpp
@@ -2,7 +2,36 @@
 
 using namespace std;
 
+// Insertion Operator
+
+class Complex
+{
+  private:
+
+    int real;
+    int img;
+
+  public:
+
+    Complex(int r = 0, int i = 0)
+    {
+      real = r;
+      img = i;
+    }
+
+    friend ostream& operator<<(ostream &o, Complex &c1);
+};
+
+ostream& operator<<(ostream &o, Complex &c1)
+{
+  cout << c1.real << " + i" << c1.img << endl;
+}
+
 int main()
 {
-  
+  Complex c1(3, 7);
+
+  int x = 10;
+  cout << x << endl;
+  cout << c1 << endl;
 }
