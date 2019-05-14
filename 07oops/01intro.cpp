9b318194e1a6a270cf9a17a3265406e6f2d85da7
Russell Koons <rkoons@gmail.com>
2019-05-14 11:51:04 -0400

    back at it

---
+++ 07oops/01intro.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+}
+
+// Most modern languages are Object Oriented Programming OOP
+  // Programming the Gov't
+  // Electric                 Water
+    // newCon()                function1
+    // close()                 function2
+    // billpay()
+  //Education                 Transport
+    // function1                function1
+    // function2                function2
+
+// Modular Programming is the other way
+// Imagine programming a Bank
+  // openAcc()
+  // deposit()
+  // withdraw()
+  // checkBal()
+  // applyloan()
---
+++ 07oops/02principles.cpp
@@ -0,0 +1,15 @@
+// Abstraction
+  // 2 elements of programming
+    // 1. Data
+
+    // 2. Functions
+  
+  // You can use Classes for abstraction
+
+// Encapsulation
+
+  // Data Hiding
+
+// Inheritance
+
+// Polymorphism
