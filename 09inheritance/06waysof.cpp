c3be42c245c5d97fe98c60cbbad6bf57fe64d30f
Russell Koons <rkoons@gmail.com>
2019-05-16 14:12:11 -0400

    ways of inheritance

--- 09inheritance/06waysof.cpp
+++ 09inheritance/06waysof.cpp
@@ -2,6 +2,46 @@
 
 using namespace std;
 
+class Parent
+{
+  private:
+
+  protected:
+
+  public:
+
+};
+
+class Child:public Parent // This gets the protected and public from Parent
+{
+  private:
+    // empty
+  protected:
+    // Protected of Parent
+  public:
+    // Public of Parent
+};
+
+class Grandchild:protected Child
+{
+  private:
+    // empty
+  protected:
+    // Protected and public of Child
+  public:
+    // empty
+};
+
+class Greatgrandchild:private Grandchild
+{
+  private:
+    // Protected and Public of Grandchild
+  protected:
+    // Empty
+  public:
+    // Empty
+};
+
 int main()
 {
   
