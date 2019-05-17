0e9494e003d1b87141dea0b23843ca5e810028c6
Russell Koons <rkoons@gmail.com>
2019-05-17 15:22:12 -0400

    abstract classes

--- 11polymorphism/04abstractclasses.cpp
+++ 11polymorphism/04abstractclasses.cpp
@@ -2,7 +2,27 @@
 
 using namespace std;
 
+// Abstract Classes
+  // If base has all concrete fns: reusability
+  // If some concrete and some virtual: reusability and polymorphism
+  // All virtual: polymorphism
+
+class Base // Abstract Class
+{
+  public:
+    void fun1() { cout << "Base fun1" << endl; }
+    virtual void fun2() = 0; // Pure vitual fn: only to achieve polymorphism
+};
+
+class Derived:public Base
+{
+  public:
+    void fun2() { cout << "Derived fun2" << endl; } // Derived must override virtual fn
+};
+
 int main()
 {
-  
+  // Base b; Can't make the object
+  Base *p = new Derived(); // Can make pointer
+  p->fun1();
 }
