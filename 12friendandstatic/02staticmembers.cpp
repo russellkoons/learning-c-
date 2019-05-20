a3e59326db781ae384bcc07fc632069ffc704cc8
Russell Koons <rkoons@gmail.com>
2019-05-20 12:57:15 -0400

    static members

--- 12friendandstatic/02staticmembers.cpp
+++ 12friendandstatic/02staticmembers.cpp
@@ -2,7 +2,35 @@
 
 using namespace std;
 
+// Static Members
+
+class Test
+{
+  private:
+    int a;
+    int b;
+
+  public:
+    static int count;
+    Test()
+    {
+      a = 10;
+      b = 10;
+      count++;
+    }
+
+    static int getCount()
+    {
+      return count;
+    }
+};
+
+int Test::count = 0; // Static vars have to declared outside the class in scope res.
+
 int main()
 {
-  
+  Test t1;
+  Test t2;
+
+  cout << t2.getCount() << endl;
 }
