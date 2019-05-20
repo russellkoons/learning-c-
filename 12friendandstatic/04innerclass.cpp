31d6f41e685c3dad804ef52aeefa72a532fe4404
Russell Koons <rkoons@gmail.com>
2019-05-20 13:25:57 -0400

    Done with friend functions

--- 12friendandstatic/04innerclass.cpp
+++ 12friendandstatic/04innerclass.cpp
@@ -2,7 +2,36 @@
 
 using namespace std;
 
+// Inner Class
+
+class Outer
+{
+  public:
+    int a = 10;
+    static int b;
+    void fun() // Can access fns and data in inner classes
+    { 
+      i.show();
+      cout << i.x << endl;
+    }
+
+    class Inner
+    {
+      public:
+        int x = 25;
+        void show()
+        {
+          cout << b << endl;
+        }
+    };
+
+    Inner i; // Must build the Inner object after class declaration
+};
+
+int Outer::b = 20;
+
 int main()
 {
-  
+  Outer o;
+  o.fun(); 
 }
