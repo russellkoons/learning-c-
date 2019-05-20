f565a5a3a8baded05f60143314ba72e00e489dee
Russell Koons <rkoons@gmail.com>
2019-05-20 13:06:05 -0400

    static members 2

--- 12friendandstatic/03staticmembers2.cpp
+++ 12friendandstatic/03staticmembers2.cpp
@@ -2,7 +2,31 @@
 
 using namespace std;
 
+// Static members 2
+
+class Car
+{
+  public:
+    static int price;
+
+    Car()
+    {
+
+    }
+
+    static int getPrice()
+    {
+      return price;
+    }
+};
+
+int Car::price = 20;
+
 int main()
 {
-  
+  cout << Car::getPrice() << endl; // Can call the function before creating an object
+
+  Car my;
+
+  cout << my.getPrice() << endl;
 }
