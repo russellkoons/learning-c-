cc4ac1d8afe5fabd5c07c1c8cd113b63bb41f112
Russell Koons <rkoons@gmail.com>
2019-05-27 14:33:17 -0400

    throw and catch

--- 13exceptionhandling/02throwandcatch.cpp
+++ 13exceptionhandling/02throwandcatch.cpp
@@ -4,5 +4,21 @@ using namespace std;
 
 int main()
 {
-  
+  int a = 10, b = 0, c;
+
+  try
+  {
+    if (b == 0)
+      throw 101; // Throwing an error to the catch
+    c = a / b;
+    cout << c << endl;
+  }
+
+  catch (int e) // Catches the thrown error
+  {
+    cout << "Division by Zero" << endl;
+    cout << "Error code: " << e << endl;
+  }
+
+  cout << "Bye" << endl;
 }
