06df362900fd5b568339cca8f293fcd94cf3098e
Russell Koons <rkoons@gmail.com>
2019-05-27 15:02:26 -0400

    all about catch

--- 13exceptionhandling/05catch.cpp
+++ 13exceptionhandling/05catch.cpp
@@ -4,5 +4,29 @@ using namespace std;
 
 int main()
 {
-  
+  int a = 10, b = 0, c;
+
+  try
+  {
+    c = a / b;
+    cout << c << endl;
+  }
+
+  catch (int e)
+  {
+    cout << "Division by Zero" << endl;
+    cout << "Error code: " << e << endl;
+  }
+
+  catch(float e) // You can multiple catches for multiple datatype errors
+  {
+
+  }
+
+  catch(...) // This one will catch any type of datatype not already defined
+  {
+
+  }
+
+  cout << "Bye" << endl;
 }
