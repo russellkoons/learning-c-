ef84671b67fd96623b6439c902837fd1161c14e3
Russell Koons <rkoons@gmail.com>
2019-05-27 14:28:17 -0400

    exception handling

--- 13exceptionhandling/01exceptionhandling.cpp
+++ 13exceptionhandling/01exceptionhandling.cpp
@@ -2,6 +2,13 @@
 
 using namespace std;
 
+// Exception Handling
+
+// Syntax Error - Compiler fixes
+// Logical Error - Debugger
+
+// User Error - Runtime Errors - Gotta make exceptions
+
 int main()
 {
   
