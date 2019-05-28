c53c0231a11f17d88202be7667cfc5f6bc41c058
Russell Koons <rkoons@gmail.com>
2019-05-28 17:27:16 -0400

    namespaces

--- 15constant/03namespaces.cpp
+++ 15constant/03namespaces.cpp
@@ -2,7 +2,27 @@
 
 using namespace std;
 
+// Namespaces
+
+namespace First
+{
+  void fun()
+  {
+    cout << "First" << endl;
+  }
+}
+
+namespace Second
+{
+  void fun()
+  {
+    cout << "Second" << endl;
+  }
+}
+
+using namespace First; // This calls the First namespace
 int main()
 {
-  
+  fun(); // Using First fun() because of above using
+  Second::fun(); // This calls the Second fun()
 }
