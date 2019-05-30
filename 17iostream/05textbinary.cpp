2f4925720158c06bd23526bc5850fb0d9481f887
Russell Koons <rkoons@gmail.com>
2019-05-30 14:37:04 -0400

    text vs binary

--- 17iostream/05textbinary.cpp
+++ 17iostream/05textbinary.cpp
@@ -2,6 +2,18 @@
 
 using namespace std;
 
+// Text Files
+  // Human Readable - int x = 23
+    // Converted into ascii codes - 50 and 51
+    // Or 00110010 and 00110011
+    // Notepad reads each 8 bits, converts to ascii, turns into digits
+  // Slower and takes up more space
+
+// Binary Files
+  // Machine Readable - 0000000000010111
+    // Need to use read() and write() functions to read binary files
+  //Faster and takes up less space
+
 int main()
 {
   
