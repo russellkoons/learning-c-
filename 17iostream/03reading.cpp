22b5c5dab30478b08253114d2a6a08f671641e70
Russell Koons <rkoons@gmail.com>
2019-05-30 14:18:24 -0400

    reading a file

--- 17iostream/03reading.cpp
+++ 17iostream/03reading.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int main()
 {
-  
+  ifstream inf;
+  inf.open("My.txt"); // Opens file
+  // if (!inf.is_open) Checks if a file is open or not, returns true or false
+  //   cout << "File cannot be opened" << endl;
+  string str;
+  int x;
+
+  inf >> str; // Grabs a string from the file
+  inf >> x; // Grabs an int
+  cout << str << " " << x << endl;
+  if (inf.eof()) // Checks if it has reached the end of the file
+    cout << "End of file" << endl;
+  inf.close(); // Closes file, of course
 }
