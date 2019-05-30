0729210a0115c17f878880952ed4970480c91d77
Russell Koons <rkoons@gmail.com>
2019-05-30 13:54:06 -0400

    streams

--- 17iostream/01streams.cpp
+++ 17iostream/01streams.cpp
@@ -2,6 +2,12 @@
 
 using namespace std;
 
+// Streams access data from outside the program
+// ios is input/output stream
+// istream = input stream; ostream = output stream;
+// ifstream = input file stream
+// ofstream = output file stream
+
 int main()
 {
   
