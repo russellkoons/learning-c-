8beec4056e1fcf3750c3a27655836a0eb4e1e6fc
Russell Koons <rkoons@gmail.com>
2019-05-30 13:48:38 -0400

    virtual destructors

--- 16destructor/02virtualdestructor.cpp
+++ 16destructor/02virtualdestructor.cpp
@@ -2,7 +2,41 @@
 
 using namespace std;
 
+// Virtual Destructor
+
+class Base
+{
+  public:
+    Base()
+    {
+      cout << "Base Constructor" << endl;
+    }
+
+    virtual ~Base() // Making it virtual makes it so it calls the destructor of the pointer
+    {
+      cout << "Base Destructor" << endl;
+    }
+};
+
+class Derived:public Base
+{
+  public:
+    Derived()
+    {
+      cout << "Derived Constructor" << endl;
+    }
+
+    ~Derived()
+    {
+      cout << "Derived Destructor" << endl;
+    }
+};
+
 int main()
 {
-  
+  Derived d;
+
+  Base *p = new Derived();
+
+  delete p;
 }
