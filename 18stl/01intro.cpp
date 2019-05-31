ea713707da3e54e3adb19bd1b21b993db5142b62
Russell Koons <rkoons@gmail.com>
2019-05-31 16:23:03 -0400

    why stl

--- 18stl/01intro.cpp
+++ 18stl/01intro.cpp
@@ -5,7 +5,10 @@ using namespace std;
 // Standard Template Library
 
   // Why STL?
-  
+    // Data Structures
+    // You keep 10 Student Marks in an array. Once created size cannot be changed.
+    // int A[10]; <- Made in Stack
+    // int *A = new int[10]; <- Made in Heap
 
 int main()
 {
