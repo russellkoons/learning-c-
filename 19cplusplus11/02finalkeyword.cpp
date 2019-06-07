f64777c437b718d1e3cda3089233654c87256e7f
Russell Koons <rkoons@gmail.com>
2019-06-07 08:33:51 -0400

    Final

---
+++ 19cplusplus11/02finalkeyword.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+// Final Keyword
+
+class Parent // final // Final makes it so you cannot inherit from this class
+{
+  virtual void show() final {} // Can only be final if it's virtual
+};
+
+class Child:Parent
+{
+  void show() {}
+};
+
+int main()
+{
+
+}
