5c1c0a71d0e496dfdb1b2cf42ff36fb779bd7d97
Russell Koons <rkoons@gmail.com>
2019-06-07 10:10:25 -0400

    data types

--- 21datatypes/01datatypes.cpp
+++ 21datatypes/01datatypes.cpp
@@ -2,6 +2,24 @@
 
 using namespace std;
 
+// Data Types
+
+// bool - boolean - undefined
+
+// char - character - 8-bits
+// wchar_t - wide char - 16-bits
+// char16_t - unicode char - 16-bits
+// char32_t - unicode char - 32-bits
+
+// short - short int - 16-bits
+// int - integer - 16-bits
+// long - long int - 32-bits
+// longlong - very long int - 64-bits
+
+// float - single precision - 32-bits
+// double - double precision - 64-bits
+// longdouble - double precision - 10/16-bits
+
 int main()
 {
   
